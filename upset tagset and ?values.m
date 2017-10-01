3211b1709a5685fee84c601ec7169725400cb37b
Ali Hashmi <ah946@cam.ac.uk>
2017-10-01 15:15:41 +0200

    Update upset tagset and ?values.m

--- upset tagset and ?values.m
+++ upset tagset and ?values.m
@@ -56,14 +56,26 @@ f[x, 3] (* if the expr has the form f[x,something], the rule above is applied*)
 f[3, x] (* if first argument is not x then nothing happens*)
 (* f[3, x] *)
 
-outside /: outside[middle[inside[xFix]]] = xFixOutside;
+outside /: outside[middle[inside@xFix]] = xFixOutside;
+DownValues[outside]
+(* {HoldPattern[outside[middle[inside[xFix]]]] :> xFixOutside} *)
+UpValues[outside]
+(* {} *)
 
 middle /: outside[middle[inside[xFix]]] = xFixMiddle;
+DownValues[middle]
+(* {} *)
+UpValues[middle]
+(* {HoldPattern[outside[middle[inside[xFix]]]] :> xFixMiddle} *)
 
 inside /: outside[middle[inside[xFix]]] = xFixInside;
 
 (* During evaluation of In[246]:= TagSet::tagpos: Tag inside in outside[middle[inside[xFix]]] is too deep\
 for an assigned rule to be found. *)
+DownValues[inside]
+(* {} *)
+UpValues[inside]
+(* {} *)
 
 xFix /: outside[middle[inside[xFix]]] = xFixInside;
 (* During evaluation of In[245]:= TagSet::tagpos: Tag xFix in outside[middle[inside[xFix]]] is too deep\
@@ -146,4 +158,76 @@ SubValues[a]
 ClearAll[a, b, c, d, e];
 b /: a[b][c][d] = e;
 (* During evaluation of In[321]:= TagSet::tagpos: Tag b in a[b][c][d] is too deep for an assigned rule to be found. *)
+
+(* DOWNVALUES *)
+DownValues[u] = {HoldPattern[u[1]] :> 2, HoldPattern[u[-1]] :> -1, HoldPattern[u[x_]]:> x^2};
+DownValues[u, Sort -> True]
+(* {HoldPattern[u[-1]] :> -1, HoldPattern[u[1]] :> 2, HoldPattern[u[x_]] :> x^2} *)
+DownValues[u, Sort -> False]
+(* {HoldPattern[u[1]] :> 2, HoldPattern[u[-1]] :> -1, HoldPattern[u[x_]] :> x^2} *)
+
+Remove[x];
+x = 4;
+Through[{DownValues,UpValues,OwnValues,NValues}[x]]
+(* {{},{},{HoldPattern[x] :> 4}, {}} *)
+
+Remove[f];
+N[f[x_]] := 6.0;
+Through[{DownValues,UpValues,OwnValues,NValues}[f]]
+(* {{},{},{},{HoldPattern[N[f[x_],{MachinePrecision, MachinePrecision}]] :> 6.}} *)
+{f[5],f[4]//N, N[f[5]], N@f[6], f[7.0], N[f[4],5], f[N[4,5]]}
+(* {f[5],6.,6.,6.,f[7.],f[4.0000],f[4.0000]} *)
+
+ClearAll[g];
+N[g[x_],digits_]:=G[N[x,digits]];
+
+??g
+(* Global`g
+N[g[x_],digits_]:=G[N[x,digits]] *)
+
+NValues[g]
+(* {HoldPattern[N[g[x_], digits_]] :> G[N[x, digits]]} *)
+N[g[1/6],10]
+(* G[0.16666666667] *)
+N[g[1/6]]
+(* G[0.166667] *)
+
+Clear[n,g,d,e];
+n[g][d][e] = n g d
+(* d g n*)
+??n
+(* Global`n
+n[g][d][e]=d g n *)
+Through[{OwnValues,DownValues,UpValues,SubValues}[n]]
+(* {{},{},{},{HoldPattern[n[g][d][e]] :> d g n}} *)
+
+Clear[n];
+n[g][d][e] ^= deriv;
+UpValues[e]
+(* {HoldPattern[n[g][d][e]] :> deriv } *)
+
+Clear[n,g,d,e];
+n/: n[g][d][e] := deriv;
+e/: n[g][d][e] := deriv;
+SubValues[n]
+(* {HoldPattern[n[g][d][e]] :> deriv} *)
+UpValues[e]
+(* {HoldPattern[n[g][d][e]] :> deriv} *)
+
+Clear[n,g,d];
+n[g][d] = n g d;
+??n
+(* Global`n
+n[g][d]=d g n
+*)
+Through[{OwnValues,UpValues,SubValues}[n]]
+(* {{}, {}, {HoldPattern[n[g][d]] :> d g n}} *)
+
+
+
+
+
+
+
+
 
