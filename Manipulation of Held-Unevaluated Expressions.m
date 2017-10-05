e651a3d11e55f20026afe3f4387d6219fb99269e
Ali Hashmi <ah946@cam.ac.uk>
2017-10-05 13:52:43 +0200

    Update Manipulation of Held-Unevaluated Expressions.m

--- Manipulation of Held-Unevaluated Expressions.m
+++ Manipulation of Held-Unevaluated Expressions.m
@@ -1,1 +1,3 @@
-(* In order  *)
+(* In order to map a functions to arguments inside held expression without evaluation *)
+func/@Unevaluated/@Hold[arg1,arg2,arg3]
+(* Hold[ func[Unevaluated@arg1],func[Unevaluated@arg2],func[Unevaluated@arg3] ] *)
