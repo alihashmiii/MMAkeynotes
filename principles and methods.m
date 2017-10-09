83aafa80305703d4766b3896d9e09629db6d2979
Ali Hashmi <ah946@cam.ac.uk>
2017-10-09 15:56:57 +0200

    Update principles and methods.m

--- principles and methods.m
+++ principles and methods.m
@@ -1,1 +1,10 @@
 (* From Robby Villegas's presentation *)
+
+(* Structural elements are your friends; they work well on unevaluated expressions. *)
+(* FLATTEN *)
+Attributes[f] = HoldAll;
+f[Xor[elems___]] := Replace[Sort@Flatten[Hold[elems],Infinity,Xor],
+Hold[args___] :> HoldForm[CirclePlus@args]];
+f[Xor[Xor[1==1,Xor[0==1]], Xor[Xor[2==1,1==0], 2==0]]]
+(* (0==1)\[CirclePlus](1==0)\[CirclePlus](1==1)\[CirclePlus](2==0)\[CirclePlus](2==1) *)
+
