2266d8319dfa58c39b6d781784503192fc3ff03d
Ali Hashmi <ah946@cam.ac.uk>
2017-10-09 16:59:13 +0200

    Update principles and methods.m

--- principles and methods.m
+++ principles and methods.m
@@ -8,8 +8,12 @@ Hold[args___] :> HoldForm[CirclePlus@args]];
 f[Xor[Xor[1==1,Xor[0==1]], Xor[Xor[2==1,1==0], 2==0]]]
 (* (0==1)\[CirclePlus](1==0)\[CirclePlus](1==1)\[CirclePlus](2==0)\[CirclePlus](2==1) *)
 
-(* *)
+(* a function that automatically flattens multiple levels of itself *)
 ClearAll[f];
 SetAttributes[f, HoldAll];
 f[args___] /; MemberQ[Unevaluated[{args}], _f] := Flatten[Unevaluated[f[args]], Infinity, f];
 f[singleton_] := singleton;
+f[elems___]:= Hold[elems];
+
+f[f[1, 2, 3], f[4], f[6^5, f[7, 8, 9]]] 
+(* Hold[1, 2, 3, 4, 6^5, 7, 8, 9] *)
