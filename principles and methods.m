562caeb603b26bc04bd4554daa18c0b8eb9d294f
Ali Hashmi <ah946@cam.ac.uk>
2017-10-09 16:16:52 +0200

    Update principles and methods.m

--- principles and methods.m
+++ principles and methods.m
@@ -8,3 +8,8 @@ Hold[args___] :> HoldForm[CirclePlus@args]];
 f[Xor[Xor[1==1,Xor[0==1]], Xor[Xor[2==1,1==0], 2==0]]]
 (* (0==1)\[CirclePlus](1==0)\[CirclePlus](1==1)\[CirclePlus](2==0)\[CirclePlus](2==1) *)
 
+(* *)
+ClearAll[f];
+SetAttributes[f, HoldAll];
+f[args___] /; MemberQ[Unevaluated[{args}], _f] := Flatten[Unevaluated[f[args]], Infinity, f];
+f[singleton_] := singleton;
