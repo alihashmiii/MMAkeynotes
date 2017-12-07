8ff298b37c7b741ae2ad01086213af324855150b
Ali Hashmi <ah946@cam.ac.uk>
2017-12-07 22:29:26 +0100

    Update and rename multigroup - example by Leonid.m to nested association - example by Leonid Shifrin.m

---
+++ nested association - example by Leonid Shifrin.m
@@ -0,0 +1,15 @@
+data = {<|"company" -> "AAPL", "date" -> {2013, 12, 26}, "open" -> 80.2231|>, <|"company" -> "AAPL", 
+    "date" -> {2013, 12, 27}, "open" -> 79.6268|>, <|"company" -> "AAPL", "date" -> {2013, 12, 30}, 
+    "open" -> 78.7252|>, <|"company" -> "AAPL",  "date" -> {2013, 12, 31}, "open" -> 78.2626|>, <|
+    "company" -> "AAPL", "date" -> {2014, 1, 2}, "open" -> 78.4701|>, <|"company" -> "AAPL", 
+    "date" -> {2014, 1, 3}, "open" -> 78.0778|>, <|"company" -> "MSFT", "date" -> {2013, 12, 26}, 
+    "open" -> 36.6635|>, <|"company" -> "MSFT", "date" -> {2013, 12, 27}, "open" -> 37.0358|>, <|
+    "company" -> "MSFT", "date" -> {2013, 12, 30}, "open" -> 36.681|>, <|"company" -> "MSFT", 
+    "date" -> {2013, 12, 31}, "open" -> 36.8601|>, <|"company" -> "MSFT", "date" -> {2014, 1, 2}, 
+    "open" -> 36.8173|>, <|"company" -> "MSFT", "date" -> {2014, 1, 3}, "open" -> 36.6658|>, <|"company" -> "GE", 
+    "date" -> {2013, 12, 26}, "open" -> 27.2125|>, <|"company" -> "GE", "date" -> {2013, 12, 27}, 
+    "open" -> 27.3698|>, <|"company" -> "GE", "date" -> {2013, 12, 30}, "open" -> 27.3708|>, <|
+    "company" -> "GE", "date" -> {2013, 12, 31}, "open" -> 27.4322|>, <|"company" -> "GE", "date" -> {2014, 1, 2}, 
+    "open" -> 27.394|>, <|"company" -> "GE", "date" -> {2014, 1, 3}, "open" -> 27.0593|>};
+
+(* lets create *)
